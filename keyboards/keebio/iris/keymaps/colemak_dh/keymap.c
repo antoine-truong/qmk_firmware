d750d7330ae6c41f3626d69518fdad1f75647227
antoine-truong <pacglouton@gmail.com>
2025-06-12 23:21:49 +0400

    new colemak with single, double tap dance + hold for grave, ctrl, alt

--- keyboards/keebio/iris/keymaps/colemak_dh/keymap.c
+++ keyboards/keebio/iris/keymaps/colemak_dh/keymap.c
@@ -5,7 +5,6 @@
 #include "rgb_matrix.h" // Include here, *outside* the other #ifdef
 #endif
 #include "matrix.h"
-#include "config.h"
 
 #define _COLEMAK 0
 #define _NUMPAD 1
@@ -13,22 +12,68 @@
 #define _RAISE 3
 #define _ADJUST 4
 
-// Tap Dance Declarations
-enum {
-    TD_LSFT_CAPS = 0,
-    TD_ESC_EQL = 1,
 
+// Tap Dance Definitions
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+
+// Tap dance enums
+enum {
+    TD_ESC_EQL = 0,
+    TD_BSLS_INS = 1,
+    TD_HH_GRV_LC_LA = 2,
+    TD_HH_RCTL_RALT = 3,
+    TD_LSFT_CAPS = 4,
+    TD_RSFT_QUOT = 5
 };
+
+// Tap Dance State
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+
+// Tap Dance Action Structure
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+
+// Tap Dance State Functions
+td_state_t cur_dance(tap_dance_state_t *state);
+
+// Function Prototypes
+// for left control and left alt
+void grv_lctl_lalt_finished(tap_dance_state_t *state, void *user_data);
+void grv_lctl_lalt_reset(tap_dance_state_t *state, void *user_data);
+// for right control and right alt
+void rctl_ralt_finished(tap_dance_state_t *state, void *user_data);
+void rctl_ralt_reset(tap_dance_state_t *state, void *user_data);
 
 // Tap Dance Actions
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
     [TD_ESC_EQL] = ACTION_TAP_DANCE_DOUBLE(KC_EQL, KC_ESC),
-
-
+    [TD_BSLS_INS] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_INS),
+    [TD_HH_GRV_LC_LA] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, grv_lctl_lalt_finished, grv_lctl_lalt_reset),
+    [TD_HH_RCTL_RALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rctl_ralt_finished, rctl_ralt_reset),
+    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
 
 };
 
+// Custom Keycodes
 enum custom_keycodes {
     LIGHT = SAFE_RANGE,
     NUMPAD
@@ -49,11 +94,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
     TD(TD_ESC_EQL),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-   TD(TD_LSFT_CAPS),  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSLS,
+            KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, TD(TD_BSLS_INS),
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-           KC_LSFT,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,   RSFT_T(KC_QUOT),
+   TD(TD_LSFT_CAPS), KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,   RSFT_T(KC_QUOT),
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-           KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_HOME,           KC_END,   KC_K,    KC_H,   KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
+TD(TD_HH_GRV_LC_LA),  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_HOME,           KC_END,   KC_K,    KC_H,   KC_COMM, KC_DOT, KC_SLSH, TD(TD_HH_RCTL_RALT),
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                           KC_BSPC,  KC_DEL, MO(_LOWER),              MO(_RAISE),  KC_ENT,  KC_SPC
                                       // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -62,9 +107,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
             _______, _______, _______, _______, _______, _______,                            KC_NUM, _______, _______, _______, _______, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            _______, _______, KC_UP,   _______, _______, _______,                            KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
+            _______, _______, KC_UP,   _______, _______, _______,                            KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-            _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                            KC_PSLS, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, _______,
+            _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                            KC_PSLS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
             _______, _______, _______, _______, _______, _______, _______,          _______, KC_PDOT, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -94,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
             _______, _______, _______, _______, _______, _______,                            _______, _______, _______, KC_INS,  _______, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-            _______, KC_RALT, KC_LWIN, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
+            _______, KC_RALT, KC_LWIN, _______, _______, _______, KC_CALC,          _______, _______, _______, _______, _______, _______, _______,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                           _______, _______, _______,                    _______, _______, _______
                                       // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -108,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
             _______, _______, _______, _______, QK_BOOT, _______,                            RGB_MOD, RGB_SPI, RGB_HUI, RGB_SAI, RGB_VAI, _______,
         //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-            KC_LWIN, _______, _______, _______, DB_TOGG, _______,_______,           _______, RGB_RMOD,RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD, _______,
+            KC_LWIN, _______, _______, _______, DB_TOGG, _______,  KC_PWR,          _______,RGB_RMOD, RGB_SPD, RGB_HUD, RGB_SAD, RGB_VAD, KC_APP ,
         //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                             _______, _______, _______,                   _______, _______, _______
                                         // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -135,20 +180,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     }
  }
 
+
+ // Custom keycode processing
  bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // tap_dance_action_t *action;
+
     switch (keycode) {
+
+/*         case TD(CT_CLN):  // list all tap dance keycodes with tap-hold configurations
+            action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+            return false;
+            break; */
        case LIGHT:
-          if (record->event.pressed) {
-             next_light_mode();
-          }
-          return false;
-          break;
+            if (record->event.pressed) {
+                next_light_mode();
+            }
+            return false;
+            break;
        case NUMPAD:
-          if (record->event.pressed) {
-             layer_invert(_NUMPAD);
-          }
-          return false;
-          break;
+            if (record->event.pressed) {
+                layer_invert(_NUMPAD);
+            }
+            return false;
+            break;
 /*        case LOWER:
           if (record->event.pressed) {
              layer_on(_LOWER);
@@ -183,6 +241,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     return true;
  }
 
+
 // Encoder support
 bool encoder_update_user(uint8_t index, bool clockwise) {
     switch (get_highest_layer(layer_state)) {
@@ -195,14 +254,14 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             break;
         case _LOWER:
             if (index == 0) {
-                tap_code(clockwise ? KC_VOLU : KC_VOLD);
+                tap_code(clockwise ? KC_BRIU  : KC_BRID);
             } else {
                 tap_code(clockwise ? KC_PGDN : KC_PGUP);
             }
             break;
         case _RAISE:
             if (index == 0) {
-                tap_code(clockwise ? KC_BRIU : KC_BRID);
+                tap_code(clockwise ? KC_VOLU: KC_VOLD);
             } else {
                 tap_code(clockwise ? KC_MPLY : KC_MUTE);
             }
@@ -220,7 +279,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
 // Add this function to enable tri-layer logic:
 void matrix_scan_user(void) {
-    update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    if (layer_state_is(_LOWER) || layer_state_is(_RAISE)) {
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+    }
 }
 
 
@@ -328,4 +389,119 @@ void set_range(uint8_t start, uint8_t stop, uint8_t led_min, uint8_t led_max, ui
     return true;
  }
 
+
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicative that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currently not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustrating to type.
+ *
+ * For the third point, there does exist the 'TD_DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    }
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+        else return TD_TRIPLE_HOLD;
+    } else return TD_UNKNOWN;
+}
+
+// Create an instance of 'td_tap_t' for the 'x' tap dance.
+static td_tap_t grv_lctl_lalt_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+static td_tap_t rctl_ralt_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// Tap Dance Functions for left control and left alt
+// This function is called when the tap dance is finished
+void grv_lctl_lalt_finished(tap_dance_state_t *state, void *user_data) {
+    grv_lctl_lalt_tap_state.state = cur_dance(state);
+    switch (grv_lctl_lalt_tap_state.state) {
+        case TD_SINGLE_TAP: register_code(KC_GRV); break;
+        case TD_SINGLE_HOLD: register_code(KC_LCTL); break;
+        case TD_DOUBLE_TAP: register_code(KC_HOME); break;
+        case TD_DOUBLE_HOLD: register_code(KC_LALT); break;
+        // Last case is for fast typing. Assuming your key is `f`:
+        // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+        // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+        // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X); break;
+        default: break;
+    }
+}
+
+// Reset function for the left control and left alt tap dance
+void rctl_ralt_finished(tap_dance_state_t *state, void *user_data) {
+    rctl_ralt_tap_state.state = cur_dance(state);
+    switch (rctl_ralt_tap_state.state) {
+        case TD_SINGLE_HOLD: register_code(KC_RCTL); break;
+        case TD_DOUBLE_HOLD: register_code(KC_RALT); break;
+        case TD_DOUBLE_TAP: register_code(KC_END); break;
+        // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X); break;
+        default: break;
+    }
+}
+// Reset function for the right control and right alt tap dance
+// This function is called when the tap dance is reset
+void grv_lctl_lalt_reset(tap_dance_state_t *state, void *user_data) {
+    switch (grv_lctl_lalt_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code(KC_GRV); break;
+        case TD_SINGLE_HOLD: unregister_code(KC_LCTL); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_HOME); break;
+        case TD_DOUBLE_HOLD: unregister_code(KC_LALT); break;
+        // case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
+        default: break;
+    }
+    grv_lctl_lalt_tap_state.state = TD_NONE;
+}
+
+// Reset function for the right control and right alt tap dance
+void rctl_ralt_reset(tap_dance_state_t *state, void *user_data) {
+    switch (rctl_ralt_tap_state.state) {
+        case TD_SINGLE_HOLD: unregister_code(KC_RCTL); break;
+        case TD_DOUBLE_HOLD: unregister_code(KC_RALT); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_END); break;
+        // case TD_DOUBLE_SINGLE_TAP: unregister_code(KC_X); break;
+        default: break;
+    }
+    rctl_ralt_tap_state.state = TD_NONE;
+}
 
